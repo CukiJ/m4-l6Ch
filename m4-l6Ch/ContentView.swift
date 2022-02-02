64a3a34400174dfec0f6dd40a7323d6006db9c5c
Jirike Cuki <sulak.jiri@gmail.com>
2022-02-02 14:06:59 +0100

    Initial Commit

---
+++ m4-l6Ch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m4-l6Ch
+//
+//  Created by Jiří Šulák on 02.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m4-l6Ch/m4_l6ChApp.swift
@@ -0,0 +1,17 @@
+//
+//  m4_l6ChApp.swift
+//  m4-l6Ch
+//
+//  Created by Jiří Šulák on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct m4_l6ChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
